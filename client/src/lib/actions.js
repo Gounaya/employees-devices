3375187d0563413fa2fd76a833ff3e4e2944ee6e
GOUNAYA Oussama <b179ry@login.axa>
2024-12-22 11:24:46 +0100

    mettre à jour le port

--- client/src/lib/actions.js
+++ client/src/lib/actions.js
@@ -2,7 +2,7 @@
 
 import { revalidatePath } from 'next/cache'
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8081'
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080'
 
 // Employee-related functions
 export async function getEmployees() {
